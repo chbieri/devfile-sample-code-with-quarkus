0322f0ceae49e45bd943e2a65d923b8aa2fc0e2b
ig-bic <ig-bic@medidata.ch>
2023-12-18 17:25:03 +0100

    add query param for name

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -3,6 +3,7 @@ package org.acme;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello-resteasy")
@@ -10,7 +11,10 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String hello(@QueryParam("name") String name) {
+        if (name != null) {
+            return "Hello " + name;
+        }
         return "Hello RESTEasy";
     }
 }
